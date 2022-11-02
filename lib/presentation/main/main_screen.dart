aa3ce2733ab7d95a049bb1647fca637df2d9b8aa
yeonjoo.lee <teddyjune@naver.com>
2022-11-03 00:40:19 +0900

    feat: FirestoreListView 구현

--- lib/presentation/main/main_screen.dart
+++ lib/presentation/main/main_screen.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_why/data/model/memo.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterfire_ui/firestore.dart';
 import 'package:go_router/go_router.dart';
 
 class MainScreen extends StatelessWidget {
@@ -24,6 +27,21 @@ class MainScreen extends StatelessWidget {
               onPressed: () => context.go('/write'),
               //go는 replace, push는 위로 올라온다.
               child: const Text('글쓰기'),
+            ),
+            Expanded(
+              child: FirestoreListView<Memo>(
+                query: FirebaseFirestore.instance.collection('memos').withConverter<Memo>(
+                  fromFirestore: (snapshot, _) => Memo.fromJson(snapshot.data()!),
+                  toFirestore: (memo, _) => memo.toJson(),
+                ),
+                itemBuilder: (context, snapshot) {
+                  final memo = snapshot.data();
+                  return ListTile(
+                    title: Text(memo.title),
+                    subtitle: Text(memo.body),
+                  );
+                },
+              ),
             ),
           ],
         ),
